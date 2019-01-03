af133590e5bbc09f3f7c1a0d4faa99c3bb49fa8c
Anselm von Wangenheim <anselm.w@gmx.de>
2019-01-03 23:06:18 +0100

    add examples

---
+++ src/example/LinearAssignmentAPI.scala
@@ -0,0 +1,54 @@
+package example
+
+import com.google.ortools.graph.LinearSumAssignment
+// Copyright 2010-2017 Google
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+/**
+  * Test assignment on a 4x4 matrix. Example taken from
+  * http://www.ee.oulu.fi/~mpa/matreng/eem1_2-1.htm with kCost[0][1] modified so the optimum solution
+  * is unique.
+  */
+object LinearAssignmentAPI {
+  private def runAssignmentOn4x4Matrix(): Unit = {
+    val numSources = 4
+    val numTargets = 4
+    val cost = Array(
+      Array(90, 76, 75, 80),
+      Array(35, 85, 55, 65),
+      Array(125, 95, 90, 105),
+      Array(45, 110, 95, 115),
+    )
+    val expectedCost = cost(0)(3) + cost(1)(2) + cost(2)(1) + cost(3)(0)
+    val assignment = new LinearSumAssignment
+    (0 until numSources).foreach(source =>
+      (0 until numTargets).foreach(target =>
+        assignment.addArcWithCost(source, target, cost(source)(target))))
+    if (assignment.solve == LinearSumAssignment.Status.OPTIMAL) {
+      println("Total cost = " + assignment.getOptimalCost + "/" + expectedCost)
+
+      (0 until assignment.getNumNodes).foreach(node =>
+        println("Left node " + node + " assigned to right node " + assignment.getRightMate(node) + " with cost " + assignment.getAssignmentCost(node)))
+    }
+    else println("No solution found.")
+  }
+
+  @throws[Exception]
+  def main(args: Array[String]): Unit = {
+    LinearAssignmentAPI.runAssignmentOn4x4Matrix()
+  }
+
+  try System.loadLibrary("jniortools")
+
+}
---
+++ src/example/LinearProgramming.scala
@@ -0,0 +1,84 @@
+package example
+
+import com.google.ortools.linearsolver.MPSolver
+import com.google.ortools.linearsolver.LinearSolverImplicits._
+
+/** Linear programming example that shows how to use the API. */
+object LinearProgramming {
+  private def createSolver(solverType: String) = try
+    new MPSolver("LinearProgrammingExample", MPSolver.OptimizationProblemType.valueOf(solverType))
+  catch {
+    case e: IllegalArgumentException =>
+      null
+  }
+
+  private def runLinearProgrammingExample(solverType: String, printModel: Boolean): Unit = {
+    val solver = createSolver(solverType)
+    if (solver == null) {
+      println("Could not create solver " + solverType)
+      return
+    }
+
+    // x1, x2 and x3 are continuous non-negative variables.
+    val x1 = solver.makeNonNegVar("x1")
+    val x2 = solver.makeNonNegVar("x2")
+    val x3 = solver.makeNonNegVar("x3")
+
+    solver.maximize(10 * x1 + 6 * x2 + 4 * x3)
+
+    val c0 = solver.addConstraint(x1 + x2 + x3 <= 100)
+    val c1 = solver.addConstraint(x1 * 10 + x2 * 4 + x3 * 5 <= 600)
+    val c2 = solver.addConstraint(2 * x1 + 2 * x2 + 6 * x3 <= 300)
+
+    println("Number of variables = " + solver.numVariables)
+    println("Number of constraints = " + solver.numConstraints)
+    if (printModel) {
+      val model = solver.exportModelAsLpFormat(false)
+      println(model)
+    }
+    val resultStatus = solver.solve
+    // Check that the problem has an optimal solution.
+    if (resultStatus != MPSolver.ResultStatus.OPTIMAL) {
+      System.err.println("The problem does not have an optimal solution!")
+      return
+    }
+    // Verify that the solution satisfies all constraints (when using solvers
+    // others than GLOP_LINEAR_PROGRAMMING, this is highly recommended!).
+    if (!solver.verifySolution(/*tolerance=*/ 1e-7, /*logErrors=*/ true)) {
+      System.err.println("The solution returned by the solver violated the" + " problem constraints by at least 1e-7")
+      return
+    }
+    println("Problem solved in " + solver.wallTime + " milliseconds")
+    // The objective value of the solution.
+    println("Optimal objective value = " + solver.objective.value)
+    // The value of each variable in the solution.
+    println("x1 = " + x1.solutionValue)
+    println("x2 = " + x2.solutionValue)
+    println("x3 = " + x3.solutionValue)
+    val activities = solver.computeConstraintActivities
+    println("Advanced usage:")
+    println("Problem solved in " + solver.iterations + " iterations")
+    println("x1: reduced cost = " + x1.reducedCost)
+    println("x2: reduced cost = " + x2.reducedCost)
+    println("x3: reduced cost = " + x3.reducedCost)
+    println("c0: dual value = " + c0.dualValue)
+    println("    activity = " + activities(c0.index))
+    println("c1: dual value = " + c1.dualValue)
+    println("    activity = " + activities(c1.index))
+    println("c2: dual value = " + c2.dualValue)
+    println("    activity = " + activities(c2.index))
+  }
+
+  @throws[Exception]
+  def main(args: Array[String]): Unit = {
+    println("---- Linear programming example with GLOP (recommended) ----")
+    runLinearProgrammingExample("GLOP_LINEAR_PROGRAMMING", true)
+    println("---- Linear programming example with CLP ----")
+    runLinearProgrammingExample("CLP_LINEAR_PROGRAMMING", false)
+    println("---- Linear programming example with GLPK ----")
+    runLinearProgrammingExample("GLPK_LINEAR_PROGRAMMING", false)
+  }
+
+  try System.loadLibrary("jniortools")
+
+}
