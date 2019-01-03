373bfdf66ad1bbad09fff8b2a06fccf49d5d118c
Anselm von Wangenheim <anselm.w@gmx.de>
2019-01-03 23:03:32 +0100

    add LinearSolverImplicits

---
+++ src/com/google/ortools/linearsolver/LinearSolverImplicits.scala
@@ -0,0 +1,72 @@
+package com.google.ortools.linearsolver
+
+import scala.language.implicitConversions
+
+object LinearSolverImplicits {
+  final val infinity = MPSolver.infinity
+
+  implicit def mpVar2Linear(mPVariable: MPVariable): Linear = prod(mPVariable, 1)
+
+  private def prod(mpv: MPVariable, d: Double) = Linear(Seq((mpv, d)))
+
+  case class Constraint(lb: Double, linear: Linear, ub: Double) {
+    def <=(ub: Double) = Constraint(lb, linear, math.min(this.ub, ub))
+
+    override def toString: String =
+      (if (lb > -infinity) lb + " <= " else "") +
+        linear.toString.drop(7).dropRight(1) +
+        (if (ub < infinity) " <= " + ub else "")
+  }
+
+  implicit class LowerBoundDouble(lb: Double) {
+    def <=(linear: Linear) = Constraint(lb, linear, infinity)
+  }
+
+  case class Linear(list: Seq[(MPVariable, Double)]) {
+    def +(other: Linear): Linear = Linear(list ++ other.list)
+    def -(other: Linear): Linear = this + Linear(other.list.map { case (mpv, d) => (mpv, -d) })
+    def *(scale: Double): Linear = Linear(list.map { case (mpv, d) => (mpv, d * scale) })
+    def <=(ub: Double) = Constraint(-infinity, this, ub)
+    @deprecated("consider switching arguments", "eternity")
+    def >=(lb: Double) = Constraint(lb, this, infinity)
+
+    override def toString: String = "Linear(" + list.map { case (mpv, d) => mpv.name + " * " + d }.mkString(" + ") + ")"
+  }
+
+  implicit class Prefactor(d: Double) {
+    def *(mPVariable: MPVariable): Linear = prod(mPVariable, d)
+  }
+
+  implicit class RichMPSolver(val solver: MPSolver) {
+    def addConstraint(c: Constraint, name: String = null): MPConstraint = {
+      val cons = if (name == null) solver.makeConstraint() else solver.makeConstraint(name)
+      cons.setBounds(c.lb, c.ub)
+      c.linear.list.foreach { case (mpv, d) =>
+        cons.setCoefficient(mpv, d)
+      }
+      cons
+    }
+
+    def maximize(linear: Linear): MPObjective = {
+      val obj = solver.objective()
+      linear.list.foreach { case (mpv, d) =>
+        obj.setCoefficient(mpv, d)
+      }
+      obj.setMaximization()
+      obj
+    }
+
+    def minimize(linear: Linear): MPObjective = {
+      val obj = solver.objective()
+      linear.list.foreach { case (mpv, d) =>
+        obj.setCoefficient(mpv, d)
+      }
+      obj.setMinimization()
+      obj
+    }
+
+    def makeNumVar(name: String): MPVariable = solver.makeNumVar(-infinity, infinity, name)
+    def makeNonNegNumVar(name: String): MPVariable = solver.makeNumVar(0, infinity, name)
+  }
+
+}
