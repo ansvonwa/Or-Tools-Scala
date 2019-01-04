725e7d29f0117fac5be82721310830152b0844a0
Anselm von Wangenheim <anselm.w@gmx.de>
2019-01-04 23:10:26 +0100

    add example with time limit

---
+++ src/example/SolveWithTimeLimitSampleSat.scala
@@ -0,0 +1,33 @@
+package example
+
+import com.google.ortools.sat.CpSolverStatus
+import com.google.ortools.sat.CpModel
+import com.google.ortools.sat.CpSolver
+
+
+/** Solves a problem with a time limit. */
+object SolveWithTimeLimitSampleSat {
+  @throws[Exception]
+  def main(args: Array[String]): Unit = { // Create the model.
+    val model = new CpModel
+    // Create the variables.
+    val numVals = 3
+    val x = model.newIntVar(0, numVals - 1, "x")
+    val y = model.newIntVar(0, numVals - 1, "y")
+    val z = model.newIntVar(0, numVals - 1, "z")
+    // Create the constraint.
+    model.addDifferent(x, y)
+    // Create a solver and solve the model.
+    val solver = new CpSolver
+    solver.getParameters.setMaxTimeInSeconds(10.0)
+    val status = solver.solve(model)
+    if (status eq CpSolverStatus.FEASIBLE) {
+      System.out.println("x = " + solver.value(x))
+      System.out.println("y = " + solver.value(y))
+      System.out.println("z = " + solver.value(z))
+    }
+  }
+
+  try System.loadLibrary("jniortools")
+
+}
