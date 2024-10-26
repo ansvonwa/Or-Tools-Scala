3e18e9c350d00cdf6a6ae7685cf56941abac0004
Anselm von Wangenheim <anselm.w@gmx.de>
2024-10-26 17:10:12 +0200

    mv+update LinearProgramming example

--- src/scala/main/LinearProgramming.scala
+++ src/scala/main/LinearProgramming.scala
@@ -1,19 +1,14 @@
-package example
+import com.google.ortools.Loader
+import com.google.ortools.linearsolver.{MPModelExportOptions, MPSolver}
+import com.google.ortools.linearsolver.LinearSolverImplicits.*
 
-import com.google.ortools.linearsolver.MPSolver
-import com.google.ortools.linearsolver.LinearSolverImplicits._
-
-/** Linear programming example that shows how to use the API. */
+/**
+ *  Linear programming example that shows how to use the API.
+ *  1:1 translation of the java example https://github.com/google/or-tools/blob/stable/examples/java/LinearProgramming.java
+ */
 object LinearProgramming {
-  private def createSolver(solverType: String) = try
-    new MPSolver("LinearProgrammingExample", MPSolver.OptimizationProblemType.valueOf(solverType))
-  catch {
-    case e: IllegalArgumentException =>
-      null
-  }
-
   private def runLinearProgrammingExample(solverType: String, printModel: Boolean): Unit = {
-    val solver = createSolver(solverType)
+    val solver = MPSolver.createSolver(solverType)
     if (solver == null) {
       println("Could not create solver " + solverType)
       return
@@ -32,30 +27,40 @@ object LinearProgramming {
 
     println("Number of variables = " + solver.numVariables)
     println("Number of constraints = " + solver.numConstraints)
+
     if (printModel) {
-      val model = solver.exportModelAsLpFormat(false)
+      val model = solver.exportModelAsLpFormat()
       println(model)
     }
-    val resultStatus = solver.solve
+
+    val resultStatus = solver.solve()
+
     // Check that the problem has an optimal solution.
     if (resultStatus != MPSolver.ResultStatus.OPTIMAL) {
       System.err.println("The problem does not have an optimal solution!")
       return
     }
+
     // Verify that the solution satisfies all constraints (when using solvers
     // others than GLOP_LINEAR_PROGRAMMING, this is highly recommended!).
     if (!solver.verifySolution(/*tolerance=*/ 1e-7, /*logErrors=*/ true)) {
-      System.err.println("The solution returned by the solver violated the" + " problem constraints by at least 1e-7")
+      System.err.println("The solution returned by the solver violated the" +
+        " problem constraints by at least 1e-7")
       return
     }
+
     println("Problem solved in " + solver.wallTime + " milliseconds")
+
     // The objective value of the solution.
     println("Optimal objective value = " + solver.objective.value)
+
     // The value of each variable in the solution.
     println("x1 = " + x1.solutionValue)
     println("x2 = " + x2.solutionValue)
     println("x3 = " + x3.solutionValue)
+
     val activities = solver.computeConstraintActivities
+
     println("Advanced usage:")
     println("Problem solved in " + solver.iterations + " iterations")
     println("x1: reduced cost = " + x1.reducedCost)
@@ -71,14 +76,15 @@ object LinearProgramming {
 
   @throws[Exception]
   def main(args: Array[String]): Unit = {
+    Loader.loadNativeLibraries()
     println("---- Linear programming example with GLOP (recommended) ----")
     runLinearProgrammingExample("GLOP_LINEAR_PROGRAMMING", true)
     println("---- Linear programming example with CLP ----")
     runLinearProgrammingExample("CLP_LINEAR_PROGRAMMING", false)
-    println("---- Linear programming example with GLPK ----")
-    runLinearProgrammingExample("GLPK_LINEAR_PROGRAMMING", false)
+//    println("---- Linear programming example with SCIP ----")
+//    runLinearProgrammingExample("SCIP", false)
+//    println("---- Linear programming example with XPRESS ----")
+//    runLinearProgrammingExample("XPRESS_LINEAR_PROGRAMMING", false)
   }
-
-  try System.loadLibrary("jniortools")
 
 }
